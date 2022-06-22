a4fa7867659157eb1bd91727a8cf4048861f677b
Lexie Barthelemess <lexieb@ou.edu>
2022-06-22 12:52:56 -0500

    hello josh!

--- poke.js
+++ poke.js
@@ -2,7 +2,6 @@ async function getPokemon() {
     let name = pokenames.options[pokenames.selectedIndex].text;
     let data = await fetch('https://pokeapi.co/api/v2/pokemon/' + name);
     let main = await data.json();
-
     let icon = main.sprites.front_default;
     document.getElementById('sprite').src = icon;
 }
